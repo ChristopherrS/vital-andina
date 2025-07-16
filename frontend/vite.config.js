a51b8c9614fcfb9a99a594c997389a8df1009baa
ChristopherrS <Christophersebastian89@gmail.com>
2025-07-15 20:02:47 -0500

    commit front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // Configuración por defecto para Vite + React
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 5173, 
